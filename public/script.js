e430eae84b0f9c196c36cf2988477d91ef85c07f
Duong Van Nhat <nhatn42003bn@gmail.com>
2024-11-29 09:46:55 +0700

    update apiurl

--- public/script.js
+++ public/script.js
@@ -1,4 +1,4 @@
-const apiUrl = 'http://localhost:3001'; // backend api
+const apiUrl = 'https://be-crud-student-yvgd.onrender.com/'; // backend api
 let students = []
 let currentPage = 1
 const pageSize = 3
